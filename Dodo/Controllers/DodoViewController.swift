bfc903a54fd9fa069fdbac9d9e9a6e9589aa5d00
Kevin Perkins <kevin.p@ITs-MacBook-Pro.home>
2018-02-13 22:01:08 -0500

    add item model to fix dequeueing issue with checkmarks

--- Dodo/Controllers/DodoViewController.swift
+++ Dodo/Controllers/DodoViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class DodoViewController: UITableViewController {
     
-    var itemArray = ["Buy eggs","Buy bread","Buy toilet paper"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let todoItems = defaults.array(forKey: "dodoArray") {
-            itemArray = todoItems as! [String]
+        
+        let newItem1 = Item(text: "Buy eggs")
+        let newItem2 = Item(text: "Buy milk")
+        let newItem3 = Item(text: "Buy bread")
+        itemArray.append(newItem1)
+        itemArray.append(newItem2)
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "dodoArray") as? [Item] {
+            itemArray = items
         }
     }
     
@@ -27,31 +35,29 @@ class DodoViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.accessoryType = item.finished ? .checkmark : .none
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].finished = !itemArray[indexPath.row].finished
+        
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        let cell = tableView.cellForRow(at: indexPath)
-        
-        if (cell?.accessoryType == .checkmark) {
-            cell?.accessoryType = .none
-        } else {
-            cell?.accessoryType = .checkmark
-        }
-        
     }
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
         var textField = UITextField()
         
         let alert = UIAlertController(title: "Add new dodo", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            let newItem = Item(text: textField.text!)
+            self.itemArray.append(newItem)
             self.tableView.reloadData()
             self.defaults.set(self.itemArray, forKey: "dodoArray")
         }
